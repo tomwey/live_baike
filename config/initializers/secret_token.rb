88472519bf68424a87d3550831e1d9ed96be5f34
Tomwey <tomwey@163.com>
2013-05-27 21:04:54 +0800

    xxx

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveBaike::Application.config.secret_token = 'a141298f8ed40fa357648d217ba6902d504482af924e4edec2f520304ced37b42d098e2bd83ddc84946fd56b07ada404e28e1d8da56fa80aac2f1cfe7a1caade'
