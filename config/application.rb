ceac697a2678c772da5514d2a1d2a1c0c8c9dcb8
Tomwey <tomwey@163.com>
2013-06-06 21:37:19 +0800

    xxx

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module LiveBaike
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/app/api)
+    # config.autoload_paths += %W(#{config.root}/app/api)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
