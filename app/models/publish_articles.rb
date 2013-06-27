40464c6af09e4cf7aa43273dd0b29e2ea8c21c70
Tomwey <tomwey@163.com>
2013-06-27 23:09:33 +0800

    xxx

--- app/models/publish_articles.rb
+++ app/models/publish_articles.rb
@@ -3,8 +3,8 @@ module PublishArticles
   def self.publish
     
     @publish = Publish.where(:publish_type => 1).order('created_at DESC').first
-    puts 'Starting...'
     if @publish
+      puts 'Starting...'
       @publish.publish
     end
     
