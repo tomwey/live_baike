25e2558143e96427c51a025175cc9b2eee505fd9
Tomwey <tomwey@163.com>
2013-07-01 21:08:29 +0800

    xxx

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,6 @@ set :output, File.join(Whenever.path, 'log', 'crontab.log')
 # set :environment, "development"
 
 every 1.day, :at => Time.zone.parse('4:00 am').localtime do 
+  puts Time.zone.parse('4:00 am').localtime + ' --------------------------'
   runner "PublishArticles.publish"
 end
