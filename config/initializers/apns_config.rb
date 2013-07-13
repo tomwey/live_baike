35efcc3198f3ef76d220ed5602f3201177457f3f
Tomwey <tomwey@163.com>
2013-07-13 22:13:14 +0800

    xxx

--- config/initializers/apns_config.rb
+++ config/initializers/apns_config.rb
@@ -1,11 +1,18 @@
 require 'apns'
-APNS.host = 'gateway.sandbox.push.apple.com'
 
-APNS.pem = "#{Rails.root}/config/cert.pem"
+host = Rails.env.development? ? "sandbox." : ""
+
+APNS.host = "gateway.#{host}push.apple.com"
+
+cert = Rails.env.development? ? "cert.pem" : "production_cert.pem"
+APNS.pem = "#{Rails.root}/config/#{cert}"
+
+# puts APNS.host + ' -- ' + APNS.pem
 
 APNS.pass = 'tomwey860818'
 
 APNS.port = 2195
+
 # 
 # # (default: feedback.sandbox.push.apple.com)
 # APNS.feedback_host = 'feedback.sandbox.push.apple.com'
