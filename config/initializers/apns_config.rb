2843018634f6410d4e5eaac4858ec0100900079a
Tomwey <tomwey@163.com>
2013-07-15 23:54:34 +0800

    xxx

--- config/initializers/apns_config.rb
+++ config/initializers/apns_config.rb
@@ -4,12 +4,12 @@ host = Rails.env.development? ? "sandbox." : ""
 
 APNS.host = "gateway.#{host}push.apple.com"
 
-cert = Rails.env.development? ? "cert.pem" : "production_cert.pem"
+cert = Rails.env.development? ? "cert.pem" : "livebaike_cert.pem"
 APNS.pem = "#{Rails.root}/config/#{cert}"
 
 # puts APNS.host + ' -- ' + APNS.pem
 
-APNS.pass = Rails.env.development? ? "tomwey860818" : ''
+APNS.pass = Rails.env.development? ? "tomwey860818" : 'KeKe2013'
 
 APNS.port = 2195
 
