7d2b6237c8dfdb1b35abd227eb83b0544726a769
Tomwey <tomwey@163.com>
2013-07-16 19:40:31 +0800

    xxx

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,4 @@
-require 'grocer'
+require 'apns'
 class Notification < ActiveRecord::Base
   attr_accessible :content
   
@@ -14,44 +14,51 @@ class Notification < ActiveRecord::Base
     DeviceInfo.all.each do |device_info|
       self.device_infos << device_info
       
-      n = Grocer::Notification.new(
-        device_token:  device_info.device_token, 
-        alert:         self.content,
-        sound:         "default"
-      )
+      # n = Grocer::Notification.new(
+      #   device_token:  device_info.device_token, 
+      #   alert:         self.content,
+      #   sound:         "default"
+      # )
+      
+      n = APNS::Notification.new(device_info.device_token, :alert => self.content, :sound => "default")
+      
       notifications << n
     end
     
-    cert = "#{Rails.root}/config/livebaike_cert.pem"
-    
-    puts cert
-    
-    pass = "KeKe20130206"
-    
-    pusher = Grocer.pusher(
-      certificate: cert,
-      passphrase:  pass,
-      gateway:     "gateway.push.apple.com",
-      port:        2195,
-      retries:     3
-    )
-    
-    notifications.each do |notification|
-      pusher.push(notification)
+    if notifications.any?
+      APNS.send_notifications(notifications)
     end
+    
+    # cert = "#{Rails.root}/config/livebaike_cert.pem"
+    # 
+    # puts cert
+    
+    # pass = "KeKe20130206"
+    
+    # pusher = Grocer.pusher(
+    #   certificate: cert,
+    #   passphrase:  pass,
+    #   gateway:     "gateway.push.apple.com",
+    #   port:        2195,
+    #   retries:     3
+    # )
+    
+    # notifications.each do |notification|
+    #   pusher.push(notification)
+    # end
     
     # Feedback
-    feedback = Grocer.feedback(
-      certificate: cert,
-      passphrase:  pass,
-      gateway:     "feedback.push.apple.com",
-      port:        2196,
-      retries:     3
-    )
-    
-    feedback.each do |attempt|
-      puts "Device #{attempt.device_token} failed at #{attempt.timestamp}"
-    end
+    # feedback = Grocer.feedback(
+    #   certificate: cert,
+    #   passphrase:  pass,
+    #   gateway:     "feedback.push.apple.com",
+    #   port:        2196,
+    #   retries:     3
+    # )
+    # 
+    # feedback.each do |attempt|
+    #   puts "Device #{attempt.device_token} failed at #{attempt.timestamp}"
+    # end
     
   end
   
